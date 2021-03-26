d86b110a7aa0562f7135352cd28fc6d557b61e57
Shivam Prakash <shivamprakash113@gmail.com>
2021-03-26 13:41:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('localhost','shivam','@12345678!','sparks_bank');
+	$conn = mysqli_connect('sql6.freemysqlhosting.net','sql6401607','tWFeC5EBGI','sql6401607','3306');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
